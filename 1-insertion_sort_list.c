c76576d0670597d3f112e67b9f7aedc266e77c31
agzsoftsi <agzsoftsi@gmail.com>
2020-02-01 17:16:34 +0000

    Task 1 fix betty Done

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -4,7 +4,7 @@
  * insertion_sort_list - function that sorts a doubly linked list of
  * integers in ascending order using the Insertion sort algorithm
  * @list: doubly linked list.
- * 
+ *
  */
 void insertion_sort_list(listint_t **list)
 {
